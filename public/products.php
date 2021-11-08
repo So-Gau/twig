9dbdeb56d45ded4c3d9e429687359e1c9a2d41d0
So-Gau <sophie.gaubert10@gmail.com>
2021-11-08 12:20:46 +0100

    quest 1 finish

---
+++ public/products.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig', ['products' => $products]);
